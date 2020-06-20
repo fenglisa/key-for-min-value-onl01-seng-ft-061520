cf608b817d98cb7749d7a3f170c081c7c94be3ea
Lisa Feng <lisafeng10@gmail.com>
2020-06-20 05:27:09 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,12 +3,12 @@
 require "pry"
 
 def key_for_min_value(name_hash)
-  values = []
+  numbers = []
   qualify = []
   i = 0
-  values << name_hash.collect {|item,value| value}
-  values = values[0]
-  while i < values.length
+  numbers << name_hash.collect {|item,number| number}
+  numbers = numbers[0]
+  while i < numbers.length
     if values[i] < values[i+1]
       qualify << values[i]
     elsif values[i] > values[i+1]
